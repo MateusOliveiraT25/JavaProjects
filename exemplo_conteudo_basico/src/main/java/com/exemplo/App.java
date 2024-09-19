305bb313f318220972574c64f9e719e1efc2b0f7
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-09-18 21:56:54 -0300

    18/09/24

---
+++ exemplo_conteudo_basico/src/main/java/com/exemplo/App.java
@@ -0,0 +1,10 @@
+package com.exemplo;
+
+
+
+public class App {
+    public static void main(String[] args) {
+        Exercicios objeto1 = new Exercicios();
+            objeto1.CalculadoraAvancada();
+        
+    }}
