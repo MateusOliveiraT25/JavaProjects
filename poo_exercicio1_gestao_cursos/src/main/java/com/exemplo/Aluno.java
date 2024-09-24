3918ce9e148001560a8c04f8a7df274497afeadf
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-09-24 18:56:06 -0300

    24/09/24

--- poo_exercicio1_gestao_cursos/src/main/java/com/exemplo/Aluno.java
+++ poo_exercicio1_gestao_cursos/src/main/java/com/exemplo/Aluno.java
@@ -1,35 +1,39 @@
 package com.exemplo;
 
-import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 
+// Anotação Lombok para gerar automaticamente os métodos getter e setter
 @Getter
 @Setter
 public class Aluno extends Pessoa implements Avaliave {
-    private String matricula;
-    private double nota;
+    private String matricula; // Atributo para armazenar a matrícula do aluno
+    private double nota;      // Atributo para armazenar a nota do aluno
 
-public Aluno (String nome, String cpf, String matricula){
-    super(nome,cpf);
-    this.matricula = matricula;
-    this.nota = 0 ;
-}
+    // Construtor que inicializa nome, CPF e matrícula do aluno
+    public Aluno(String nome, String cpf, String matricula) {
+        super(nome, cpf); // Chama o construtor da classe pai (Pessoa)
+        this.matricula = matricula; // Inicializa a matrícula
+        this.nota = 0; // Inicializa a nota como 0
+    }
 
-//metodo exibirinfo
-@Override
-public String exibirInfo(){
-    super.exibirInfo();
-    return " Matricula: " + matricula + " Nota: " + nota;
-}
+    // Método para exibir informações do aluno
+    @Override
+    public String exibirInfo() {
+        // Chama o método da classe pai e adiciona informações específicas do aluno
+        return super.exibirInfo() + " | Matrícula: " + matricula + " | Nota: " + nota;
+    }
 
-@Override
-public String avaliarDesempenho() {
-    if(nota>=7){
-        return "Aprovado";
-    }else if (nota>=5){
-        return "Recuperação";
-    }else 
-    return "Reprovado";
-}
+    // Método para avaliar o desempenho do aluno
+    @Override
+    public String avaliarDesempenho() {
+        // Retorna o status de aprovação com base na nota
+        if (nota >= 7) {
+            return "Aprovado"; // Aluno aprovado
+        } else if (nota >= 5) {
+            return "Recuperação"; // Aluno em recuperação
+        } else {
+            return "Reprovado"; // Aluno reprovado
+        }
+    }
 }
